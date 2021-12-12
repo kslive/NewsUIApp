19e745df3fdf7047cdb07cdfcfe8de101d27096d
Eugene <Kiselev>
2021-12-12 13:27:58 +0300

    Initial Commit

---
+++ NewsUIApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewsUIApp
+//
+//  Created by Eugene Kiselev on 12.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewsUIApp/NewsUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsUIAppApp.swift
+//  NewsUIApp
+//
+//  Created by Eugene Kiselev on 12.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct NewsUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
